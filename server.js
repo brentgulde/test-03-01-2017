344f6834decbff800196542ab2ed9bc47cd382b7
brentgulde <brentgulde@gmail.com>
2017-03-01 20:46:46 +0800

    Update server.js

--- server.js
+++ server.js
@@ -11,6 +11,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', function(req, res){
   res.sendFile(path.join(__dirname, 'views/index.html'));
 });
+app.get('/calculator', function(req, res)
+{
+	res.sendFile(path.join(__dirname, 'views/calculator.html'))
+});
 app.get('/about', function(req, res){
   res.sendFile(path.join(__dirname, 'views/about.html'));
 });
